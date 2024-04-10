f6f4f83995e8089a63031a7985dec07099dd82cf
lts-po <po@longterm.io>
2024-04-10 14:30:01 +0200

    add tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	purge: ['./src/**/*.js'],
+  content: [
+		'./src/**/*.js',
+	],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
