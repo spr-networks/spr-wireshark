9488c1fabd2acc7a3f7eb2ff6ec02c4a3916bfa8
Alex Rad <alex@supernetworks.org>
2026-07-22 12:48:34 -0700

    Run Wireshark on the host

--- go/wireshark_plugin.go
+++ go/wireshark_plugin.go
@@ -18,7 +18,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var UNIX_PLUGIN_LISTENER = "/run/spr-krun-plugin/spr-wireshark.sock"
+var UNIX_PLUGIN_LISTENER = "/state/plugins/spr-wireshark/socket"
 
 func httpInternalError(msg string, err error, w http.ResponseWriter) {
 	fmt.Println(msg, err)
